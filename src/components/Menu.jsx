cff4a3211cbe50d3a7ea90de46f0474e33296e56
DSW <elmcwl@gmail.com>
2023-08-13 16:09:13 -0700

    feat listado menu header

---
+++ src/components/Menu.jsx
@@ -0,0 +1,30 @@
+function Menu(){
+  return (
+    <header className="App-header">
+       <nav>
+        <ul>
+          <li>
+            <a href="#">
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              Personajes
+            </a>
+          <li>
+            <a href="#">
+              Episodios
+            </a>
+          </li>
+            <a href="#">
+              Citas
+            </a>
+          </li>
+        </ul>
+       </nav>
+      </header>
+  )
+}
+
+export default Menu
