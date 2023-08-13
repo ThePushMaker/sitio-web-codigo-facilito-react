d5c943bdd3a77401d826bceafbf14516712d5a85
DSW <elmcwl@gmail.com>
2023-08-13 16:58:48 -0700

    manejar rutas de forma manual con window.location.pathname

--- src/App.js
+++ src/App.js
@@ -2,10 +2,28 @@ import './App.css';
 import Menu from './components/Menu';
 
 function App() {
+  const currentURL = window.location.pathname;
+  function renderContent () {
+    switch(currentURL) {
+      case "/":
+        break;
+      case "/personajes":
+        break;
+      case "/citas":
+        break;
+      case "/episodios":
+        break;
+      default:
+        break;
+    }
+  }
+
   return (
     <div className="App">
       <Menu />
-      
+      {
+        renderContent()
+      }
     </div>
   );
 }
