b720864c1d286c386cce2b7f1eaa7a18d9aa6d04
DSW <elmcwl@gmail.com>
2023-08-16 01:09:56 -0700

    feat: replace rute syste with react router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
 import './App.css';
-import Home from './components/Home/Home.jsx'
-import Menu from './components/Menu/Menu';
-import Personajes from './components/Personajes/Personajes';
-import Episodios from './components/Episodios/Episodios';
-import Citas from './components/Citas';
+// import Home from './components/Home/Home.jsx'
+// import Menu from './components/Menu/Menu';
+// import Personajes from './components/Personajes/Personajes';
+// import Episodios from './components/Episodios/Episodios';
+// import Citas from './components/Citas';
 
 function App() {
-  const currentURL = window.location.pathname;
-  function renderContent () {
-    switch(currentURL) {
-      case "/":
-        return <Home />
-      case "/personajes":
-        return <Personajes/>
-      case "/citas":
-        return <Citas/>
-      case "/episodios":
-        return <Episodios/>
-      default:
-        return <Home/>
-    }
-  }
+  // const currentURL = window.location.pathname;
+  // function renderContent () {
+  //   switch(currentURL) {
+  //     case "/":
+  //       return <Home />
+  //     case "/personajes":
+  //       return <Personajes/>
+  //     case "/citas":
+  //       return <Citas/>
+  //     case "/episodios":
+  //       return <Episodios/>
+  //     default:
+  //       return <Home/>
+  //   }
+  // }
 
   return (
     <div className="App">
       <Menu />
       <main>
-        {renderContent()}
+        {/* {renderContent()} */}
       </main>
       <footer>
         Hecho para el taller de código facilito
--- src/index.js
+++ src/index.js
@@ -3,6 +3,32 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createHashRouter } from 'react-router-dom';
+
+import Home from './components/Home/Home.jsx'
+import Menu from './components/Menu/Menu';
+import Personajes from './components/Personajes/Personajes';
+import Episodios from './components/Episodios/Episodios';
+import Citas from '
+
+const router = createHashRouter([
+  {
+    path:'/',
+    elements:<Home />
+  }
+  {
+    path:'/personajes',
+    elements:<Personajes />
+  }
+  {
+    path:'/episodios',
+    elements:<Episodios />
+  }
+  {
+    path:'/citas',
+    elements:<Citas />
+  }
+])
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
